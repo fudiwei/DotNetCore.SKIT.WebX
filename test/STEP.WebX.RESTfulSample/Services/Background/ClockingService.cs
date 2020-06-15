0134c98db58bb9c22624c8921d1d4e98c20a2f1e
fudiwei <fudiwei@sina.com>
2020-06-15 13:44:27 +0800

    test: fixed typo

--- test/STEP.WebX.RESTfulSample/Services/Background/ClockingService.cs
+++ test/STEP.WebX.RESTfulSample/Services/Background/ClockingService.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
-namespace STEP.WebX.Sample.Services.Background
+namespace STEP.WebX.RESTfulSample.Services.Background
 {
     class ClockingService : BackgroundSchedulerService
     {
