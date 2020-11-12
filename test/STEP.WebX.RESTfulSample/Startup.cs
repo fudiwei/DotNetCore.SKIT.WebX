20d8ce455b7a378baca20c15b04ce1a3b66bcd5a
Fu Diwei <fudiwei@sina.com>
2020-11-12 14:16:33 +0800

    test: sample in .NET 5.0

--- test/STEP.WebX.RESTfulSample/Startup.cs
+++ test/STEP.WebX.RESTfulSample/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace STEP.WebX.RESTfulSample
 {
@@ -11,29 +10,20 @@ namespace STEP.WebX.RESTfulSample
 
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        private IConfiguration Configutaion { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configutaion = configuration;
         }
 
-#if !NETCOREAPP3_0
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-#else
         public void ConfigureServices(IServiceCollection services)
-#endif
         {
             services.AddWebXRESTful();
             services.AddHostedServices();
-
-#if !NETCOREAPP3_0
-            return services.BuildServiceProvider();
-#endif
         }
 
-#if !NETCOREAPP3_0
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-#else
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-#endif
         {
             app.UseWebXRESTful();
         }
